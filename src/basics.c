45f76373c7dee50a2226ef52e989ac7de40c1ff3
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-11-09 09:45:13 +0000

    press escape to quit app

--- src/basics.c
+++ src/basics.c
@@ -1,10 +1,28 @@
 #define SOKOL_IMPL
 #include "sokol_app.h"
 
+static void init(void) {}
+
+static void frame(void) {}
+
+static void cleanup(void) {}
+
+static void event(const sapp_event *event) {
+    if (event->type == SAPP_EVENTTYPE_KEY_DOWN) {
+        if (event->key_code == SAPP_KEYCODE_ESCAPE) {
+            sapp_quit();
+        }
+    }
+}
+
 sapp_desc sokol_main(int argc, char *argv[]) {
     return (sapp_desc){
         .window_title = "Basic Sokol App",
         .width = 800,
         .height = 600,
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .event_cb = event,
     };
 }
